eb6e4dc47787de11d4fd95e6d5965cb0f970e801
mguycooper <mguycooper@gmail.com>
2019-05-27 19:34:47 -0700

    Add files via upload

--- rasterinterp.m
+++ rasterinterp.m
@@ -1,12 +1,12 @@
 function Zq = rasterinterp(Z,R,Rq,method)
 
-%RASTERINTERP Interpolate the spatially referenced raster Z associated with
-% the map/geographic raster reference object R onto a new grid Zq defined
-% by map/geographic raster reference object Rq. Default interpolation is
-% 'bilinear', user specifies alternate methods 'nearest', 'cubic',
-% or 'spline' following syntax of geointerp.m / mapinterp.m (Copyright 2016
-% The MathWorks, Inc.). Works with planar (projected) and geographic data,
-% but R and Rq must both be planar or both be geographic.
+%RASTERINTERP rasterinterp(Z,R,Rq,method) Interpolate spatially referenced
+%raster Z associated with map/geographic raster reference object R onto new
+%grid Zq defined by map/geographic raster reference object Rq. Default
+%interpolation is 'bilinear', user specifies alternate methods 'nearest',
+%'cubic', or 'spline' following syntax of geointerp.m / mapinterp.m
+%(Copyright 2016 The MathWorks, Inc.). Works with planar (projected) and
+%geographic data, but R and Rq must both be planar or both be geographic.
 
 %   This function is a wrapper for the in-built Matlab functions
 %   mapinterp.m and geointerp.m (Copyright 2016 The MathWorks, Inc.), 
@@ -18,6 +18,9 @@ function Zq = rasterinterp(Z,R,Rq,method)
 %   and/or post-processing for gridded dataset i/o
 
 %   Author: Matt Cooper, guycooper@ucla.edu, May 2019
+%   Citation: Matthew Cooper (2019). matrasterlib
+%   (https://www.github.com/mguycooper/matrasterlib), GitHub. Retrieved MMM
+%   DD, YYYY.
 
 %   Syntax
 
@@ -26,14 +29,14 @@ function Zq = rasterinterp(Z,R,Rq,method)
 
 %   Description
 
-%   Zq = rasterinterp(Z,R,Rq) interpolates the spatially referenced
-%   raster Z onto a new spatially referenced raster Zq. R is a map or 
-%   geographic raster reference object, which specifies the location and 
-%   extent of data in Z.
-%   Rq is a map or geographic raster reference object, which specifies the 
-%   location and extent of data in Zq. The interpolated values in Zq 
-%   correspond to each grid-cell centroid. Extrapolation is not supported;
-%   NaN is returned for spatial locations in Zq outside the limits of Z
+%   Zq = rasterinterp(Z,R,Rq) interpolates the spatially referenced raster
+%   Z onto a new spatially referenced raster Zq. R is a map or geographic
+%   raster reference object, which specifies the location and extent of
+%   data in Z. Rq is a map or geographic raster reference object, which
+%   specifies the location and extent of data in Zq. The interpolated
+%   values in Zq correspond to each grid-cell centroid. Extrapolation is
+%   not supported; NaN is returned for spatial locations in Zq outside the
+%   limits of Z
 
 %   Zq = rasterinterp(...,method) specifies alternate methods. The default
 %   is linear interpolation. Available methods are:
@@ -47,22 +50,18 @@ function Zq = rasterinterp(Z,R,Rq,method)
 
 %   Examples
 %   
-%   In practice, rasterinterp is expected to be used with external geotiff
-%   data files that are read into matlab using geotiffread, which returns
-%   the Map/Geographic Cells Reference object required as input to
-%   rasterinterp. For example, one might have a raster dataset representing
-%   topography, and a second raster dataset representing a climate variable
-%   such as air temperature. The two rasters overlap, but have different
-%   spatial extents and/or resolution. Use rasterinterp to co-register the
-%   two rasters to the same spatial extent and resolution so they can be
-%   compared on a cell by cell basis, which requires interpolation to a new
-%   grid. The new grid can be the underlying grid of one of the rasters, or
-%   it can be a new grid altogether. In the examples below, the in-built
-%   'topo' dataset is used, and the Geographic Cells Reference object is
-%   built from scratch to illustrate the flexibility of rasterinterp, which
-%   can be used to subset or resample one raster to the extent and/or
-%   resolution of a second raster, or can be used to subset or resample
-%   one raster to any desired spatial extent or resolution.
+%   rasterinterp requires as input the Map/Geographic Cells Reference
+%   object. This object is returned by geotiffread but can also be built if
+%   the lat/lon limits and cell size are known, as in the examples below.
+%   rasterinterp can be used to co-register two rasters that overlap to the
+%   same spatial extent and resolution so they can be compared on a cell by
+%   cell basis, which requires interpolation to a new grid. The new grid
+%   (Zq) is defined by Rq, which can be the Map/Geographic Cells Reference
+%   object of one of the two rasters, used as a template, or it can be a
+%   new grid altogether. In the examples below, the in-built 'topo' dataset
+%   is used and the Geographic Cells Reference object is built from
+%   scratch. rasterinterp is used to both subset and resample 'topo' to a
+%   new spatial extent and a new resolution.
 
 %   Example 1 
 %   ---------
@@ -93,11 +92,15 @@ function Zq = rasterinterp(Z,R,Rq,method)
 
 %% Check inputs
 
-% Confirm Z is a numeric or logical grid of size R.RasterSize
+% confirm mapping toolbox is installed
+assert(license('test','map_toolbox')==1, ...
+                        'rasterinterp requires Matlab''s Mapping Toolbox.')
+
+% confirm Z is a numeric or logical grid of size R.RasterSize
 validateattributes(Z,   {'numeric', 'logical'}, ...
                         {'size', R.RasterSize}, 'rasterinterp', 'Z', 1)
                     
-% Confirm R and Rq are either MapCells or GeographicCellsReference objects
+% confirm R and Rq are either MapCells or GeographicCellsReference objects
 validateattributes(R, ...
                         {'map.rasterref.MapCellsReference', ...
                         'map.rasterref.GeographicCellsReference'}, ...
@@ -107,7 +110,7 @@ validateattributes(Rq, ...
                         'map.rasterref.GeographicCellsReference'}, ...
                         {'scalar'}, 'rasterinterp', 'Rq', 3)
                     
-% Set interpolation to 'linear' or as user-defined
+% set interpolation to 'linear' or as user-defined
 if nargin < 4
     method          =   'linear';
 else
@@ -116,27 +119,27 @@ else
                         'rasterinterp', 'method');
 end
 
-% Confirm R and Rq are either both planar or both geographic
+% confirm R and Rq are either both planar or both geographic
 assert(strcmp(R.CoordinateSystemType,Rq.CoordinateSystemType), ...
                         ['R and Rq must both be planar or both be ' ...
                         'geographic coordinate systems. Re-projection ' ...
                         'on the fly is not supported at this time']);
                     
-% Confirm R and Rq are oriented in the same N/S direction
+% confirm R and Rq are oriented in the same N/S direction
 assert(strcmp(R.ColumnsStartFrom,Rq.ColumnsStartFrom), ...
                         ['R and Rq must be oriented in the same N/S ' ...
                         'direction. Check the ''ColumnsStartFrom'' ' ...
                         'property in the Map/Geographic Cells Reference ' ... 
                         'object']);
                     
-% Confirm R and Rq are oriented in the same E/W direction
+% confirm R and Rq are oriented in the same E/W direction
 assert(strcmp(R.RowsStartFrom,Rq.RowsStartFrom), ...
                         ['R and Rq must be oriented in the same E/W ' ...
                         'direction. Check the ''RowsStartFrom'' ' ...
                         'property in the Map/Geographic Cells Reference ' ... 
                         'object']);                    
                     
-% If both R and Rq are planar, call the appropriate function
+% if both R and Rq are planar/geographic, call the appropriate function
 if strcmp(R.CoordinateSystemType,'planar')
     Zq              =   maprasterinterp(Z,R,Rq,method);
 elseif strcmp(R.CoordinateSystemType,'geographic')
@@ -164,7 +167,7 @@ end
         Xq          =   reshape(X,size(X,1)*size(X,2),1);
         Yq          =   reshape(Y,size(Y,1)*size(Y,2),1);
 
-        % call mapinterp and reshape back into a grid, Zq
+        % call mapinterp and reshape back into the new grid Zq
         Zq          =   mapinterp(Z,R,Xq,Yq,method);
         Zq          =   reshape(Zq,length(yq),length(xq)); 
         
@@ -200,7 +203,7 @@ end
         LONq        =   reshape(LON,size(LON,1)*size(LON,2),1);
         LATq        =   reshape(LAT,size(LAT,1)*size(LAT,2),1);
         
-        % call geointerp and reshape back into a grid, Zq
+        % call geointerp and reshape back into the new grid Zq
         Zq          =   geointerp(Z,R,LATq,LONq,method);
         Zq          =   reshape(Zq,length(latq),length(lonq)); 
         
